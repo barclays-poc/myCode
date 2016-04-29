746c070aebcb713921ee1fc2bfc1134d7225b044
jmjfenwick <jmjfenwick@gmail.com>
2016-04-29 20:34:28 +0100

    Refactor
    1. Changed platform to asset
    2. Created JavaScript load function

--- web/resources/javascript/data.js
+++ web/resources/javascript/data.js
@@ -1,83 +1,10 @@
-function init()
+function init(assetId, tutorialId)
 {
     populatePlatforms();
-    populateSections(null);
+    populateAsset(assetId);
+    populateTutorial(tutorialId)
 }
 
-function populateSections( assetId)
-{
-	if(assetId != null)
-	{
-        $("#webbody").slideDown("slow");
-
-        $.getJSON("http://localhost:8888/api/contents/"+ assetId, function(data) {
-        populateSection('requirements', data.requirements.content, data.requirements.information, data.requirements.additional, data.requirements.tutorial);
-        populateSection('design', data.design.content, data.design.information, data.design.additional, data.design.tutorial);
-        populateSection('build', data.build.content, data.build.information, data.build.additional, data.build.tutorial);
-        populateSection('test', data.test.content, data.test.information, data.test.additional, data.test.tutorial);
-        })}else {
-            $("#webbody").slideUp("slow");
-	}
-}
-
-function populateSection(section, content, information, additional, tutorial)
-{
-    populateMenu(section + '-information-links', information);
-    populateMenu(section + '-additional-links', additional);
-    populateMenu(section + '-tutorial-links', tutorial);
-    populateContent(section + '-content', content);
-}
-
-function populateMenu(divName, links)
-{
-    var div = document.getElementById( divName );  
-   
-   /* loops through to remove old content*/
-   while (div.hasChildNodes()) {
-       div.removeChild(div.lastChild);
-    }
-
-    /* Loops the data and creates links */
-    if(links!=null && links.length > 0)
-    {
-        for (i = 0; i < links.length; i++) 
-        { 
-            var anchor = document.createElement('a');
-            anchor.innerHTML = links[i].title;
-
-            anchor.setAttribute('href', links[i].url);
-            if (typeof links[i].target != 'undefined')
-            {
-                anchor.setAttribute('target', links[i].target);    
-            }
-            
-            if(typeof links[i].tutorialId != 'undefined')
-            {
-                click = "populateTutorial(" + links[i].tutorialId + ");"
-                anchor.setAttribute('onclick', click)
-            }
-            
-            div.appendChild( anchor );
-        }
-    }
-}
-
-function populateContent(divName, content)
-{
-    var div = document.getElementById( divName );  
-    div.innerHTML = content;
-}
-
-function populateTutorial(id)
-{   
-    <!-- API Endpoint-->
-    var url = "/data/tutorials/" + 1 + "/content.json";
-       
-    $.getJSON( url, function( data ) 
-    {
-        <!-- Bind here -->
-    });
-}
 
 function populatePlatforms()
 {
@@ -95,11 +22,43 @@ function populatePlatforms()
         function(event) {
             // The clicked node is 'event.node'
             var node = event.node;
-            if( node.assetId != 'undefined')
+            if( node.tutorialId != 'undefined')
             {
-                populateSections( node.assetId );
+                populateAsset(node.parent.assetId);
+                populateTutorial(node.tutorialId)
             }
         }
     );
 }
 
+
+function populateAsset(id)
+{   
+    if(id != null)
+    {
+        <!-- Provide API Endpoint-->
+        var url = "/data/asset.json";
+
+        $.getJSON( url, function( data ) 
+        {
+            <!-- Bind here -->
+            alert(data);
+        });
+    }
+}
+
+
+function populateTutorial(id)
+{   
+    if(id != null)
+    {
+        <!-- Provide API Endpoint-->
+        var url = "/data/tutorial.json";
+
+        $.getJSON( url, function( data ) 
+        {
+            <!-- Bind here -->
+            alert(data);
+        });
+    }
+}
