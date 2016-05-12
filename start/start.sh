f24d227e9543ea3629b75609327fcb4a15f07c1b
jmjfenwick <jmjfenwick@gmail.com>
2016-05-12 22:59:33 +0100

    Dynamic Data Load
    1. Created Mongo collections based on directories
    2. Loop files in directory and load to specified collection

--- start/start.sh
+++ start/start.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
+
   echo "Starts MongoDB"
   mongod --fork --logpath /var/log/mongod.log
+
 # Drops the current objectes==s
   echo "Removes previous database state"
   mongo api --eval "db.dropDatabase();"
-  mongo api --eval "db.tutorials.drop();"
-  mongo api --eval "db.tutorial.drop();"
+
+# Sets the working directory
+  cd "/usr/data"
+
+# Deletes the collections
+  for d in */ ; do
+    mongo api --eval "db.${d%?}.drop();"
+    echo "Removed collection ${d%?}"
+  done
+
 # Connects to the API DB
   echo "Creates the database"
   mongo --eval "connect(\"localhost:27017/api\");"
+
 # Creates the colleactions in the DB
-  echo "Creates the collections"
-  mongo api --eval "db.createCollection(\"tutorials\");"
-  mongo api --eval "db.createCollection(\"tutorial\");"
-# Need to generate this dynamically based on file lists
-  echo "Loads the data into the API database"
-  mongoimport --db api --collection "tutorials" --type "json" --file "/usr/data/tutorials.json"
-  mongoimport --db api --collection "tutorial" --type "json" --file "/usr/data/tutorial.json"
+  for d in */ ; do
+
+    #Creates collection
+    mongo api --eval "db.createCollection(\"${d%?}\");"
+    echo "Created collection ${d%?}"
+
+    #Loads the data files into the collection
+    for f in ${d%?}/* ; do
+        mongoimport --db api --collection "${d%?}" --type "json" --file "/usr/data/$f"
+        echo "Loaded $f into ${d%?}"
+    done
+
+  done
+
 # Finally starts the API - This will fail if DB not started
   echo "Starts the API listening for requests"
   nodejs /usr/api/app.js
