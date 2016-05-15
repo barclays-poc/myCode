37f8a1cb31e267a1600fa886c793710c4ac91942
jmjfenwick <jmjfenwick@gmail.com>
2016-05-15 10:11:19 +0100

    Added a new example theme
    Added a new example theme which highlighted some isses that need to be
    corrected n config

--- web/config.js
+++ web/config.js
@@ -1,6 +1,18 @@
 var config = {
     "version": 1.0,
-    "theme": "default",
     "baseUrl": "http://192.168.99.100:8080",
-    "title": "Barclays myCode"
+
+    // Barclays Theme
+    "theme": "barclays",
+    "editorTheme" : "textmate",
+    "title": "Barclays myCode",
+    "highlight": "#f6bbb5"
+
+    // Rise Theme
+    /*
+    "theme": "rise",
+    "editorTheme" : "idle_fingers",
+    "title": "Rise myCode",
+    "highlight": ""
+    */
 };
